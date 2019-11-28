2e6e38dfe95f4b9589496f0897599c20857036dc
mkcnelis <maikel.nelis@gmail.com>
2019-11-28 13:32:35 +0100

    pulled the portofolio code

---
+++ portofolio_Github-master/JavaSript_files/script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', 
+    function(){
+      Typed.new('.entry', {
+        strings: ["Portofolio Pierre", "Portofolio Damiën", "Portofolio Maikel.", "Portfolio"],
+        typeSpeed: 1
+      });
+  });
+  
+
+
