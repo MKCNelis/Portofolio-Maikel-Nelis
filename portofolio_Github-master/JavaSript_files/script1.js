2b5ea63a9e395da571132288487cc250c1c877e2
mkcnelis <maikel.nelis@gmail.com>
2019-12-10 18:23:20 +0100

    update

--- portofolio_Github-master/JavaSript_files/script1.js
+++ portofolio_Github-master/JavaSript_files/script1.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', 
     function(){
       Typed.new('.entry', {
-        strings: [ "Portfolio",],
+        strings: [ "Portfolio"+"<br>"+" maikel",],
         typeSpeed: 1
       });
   });
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded',
   document.addEventListener('DOMContentLoaded', 
   function(){
     Typed.new('.entry2', {
-      strings: [ " Maikel",],
+      strings: [ " ​Mijn naam is Maikel Kriangsak Chandeduangdee Nelis, ik ben geboren op 21 November 1997 in Bangkok, Thailand",],
       typeSpeed: 1
     });
 });
