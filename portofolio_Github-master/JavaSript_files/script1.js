dab32434f47f1b2d8d662af814c1dea5690763d8
mkcnelis <maikel.nelis@gmail.com>
2019-12-14 17:15:40 +0100

    Update script1.js
     comments

--- portofolio_Github-master/JavaSript_files/script1.js
+++ portofolio_Github-master/JavaSript_files/script1.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded',
   
 
 
-  document.addEventListener('DOMContentLoaded', 
+  document.addEventListener('DOMContentLoaded', //the second  ling of entries of scroll text
   function(){
     Typed.new('.entry2', {
       strings: [ " ​Mijn naam is Maikel Kriangsak Chandeduangdee Nelis, ik ben geboren op 21 November 1997 in Bangkok, Thailand",],
